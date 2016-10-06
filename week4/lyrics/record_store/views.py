7d8036bc1049ea133fdb14e9bb3182520b7d8387
StillsSma <samdawson301@live.com>
2016-10-06 11:38:29 -0400

    added index view and template

--- week4/lyrics/record_store/views.py
+++ week4/lyrics/record_store/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from record_store.models import Song
+import datetime
 # Create your views here.
+def index_view(request):
+    context = {
+        "my_name": "Joel Money Yeeeeyah!!!",
+        "now": datetime.datetime.now(),
+        "all_songs": Song.objects.all()
+    }
+    return render(request, "index.html", context)
