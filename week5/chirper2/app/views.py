22811adce10ba5b6ceff50c000ff2bad1962c03d
StillsSma <samdawson301@live.com>
2016-10-13 09:51:45 -0400

    added first class based view

--- week5/chirper2/app/views.py
+++ week5/chirper2/app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from app.models import Chirp
 from app.forms import ChirpForm
+from django.views import View
 # Create your views here.
 
 def index_view(request):
@@ -20,3 +21,13 @@ def about_view(request):
     print("sup" + "="*20)
     message = request.GET.get("message", "")
     return render(request, "about.html")
+
+
+
+class ChirpView(View):
+
+    def get(self, request):
+        return render(request, "chirps.html")
+
+    def post(self,request):
+        return render(request, "chirps.html")
--- week5/chirper2/chirper2/urls.py
+++ week5/chirper2/chirper2/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-from app.views import index_view, about_view
+from app.views import index_view, about_view, ChirpView
 
 
 urlpatterns = [
@@ -23,5 +23,6 @@ urlpatterns = [
     url(r'^$', index_view, name="index_view"),
     url(r'^about/$', about_view, name="about_view"),
     url(r'^', include('django.contrib.auth.urls')),
+    url(r'^chirps/$', ChirpView.as_view(), name="chirp_view")
 
 ]
