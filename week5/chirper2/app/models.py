358c075b06d9762b62916a69da96646155364db4
StillsSma <samdawson301@live.com>
2016-10-13 09:34:28 -0400

    Commited to soon

--- week5/chirper2/app/models.py
+++ week5/chirper2/app/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here.
 
 class Chirp(models.Model):
-    body = models.TextField(max_length=140)
+    body = models.CharField(max_length=140)
     created = models.DateTimeField(auto_now_add=True)
