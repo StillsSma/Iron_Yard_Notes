29446366833acb5264c62814ad5d669407642860
StillsSma <samdawson301@live.com>
2016-10-27 09:47:24 -0400

    hyperlink in models

--- week7/coffeehouse/menu_api/models.py
+++ week7/coffeehouse/menu_api/models.py
@@ -2,7 +2,17 @@ from django.db import models
 
 # Create your models here.
 
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=50)
+    calories = models.IntegerField()
+
 class Special(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField()
     price = models.FloatField()
+    ingredients = models.ManyToManyField(Ingredient)
+
+    @property
+    def calorie_count(self):
+        return 10
