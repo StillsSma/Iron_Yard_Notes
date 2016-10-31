3a13b5b0baeed6cf3e5b24ddd08a67d6c70782aa
StillsSma <samdawson301@live.com>
2016-10-31 13:50:29 -0400

    day1 coffeehouse images

--- week8/coffeehouse/menu/models.py
+++ week8/coffeehouse/menu/models.py
@@ -1,8 +1,13 @@
 from django.db import models
 
 class Special(models.Model):
-    user = models.ForeignKey("auth.User")
+    created_user = models.ForeignKey("auth.User")
     title = models.CharField(max_length=50)
     description = models.TextField()
     created_time = models.DateTimeField(auto_now_add=True)
-    
+    picture = models.FileField()
+
+    @property
+    def image_url(self):
+        if self.picture:
+            return self.picture.url 
